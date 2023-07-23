d8eee95fac9df5590c8334cc0ffd0cb4e46d347a
Tetiana Pesotska <verhulev81@gmail.com>
2023-07-23 18:46:48 +0300

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,15 @@
-import contacts from "../models/contacts.js";
+import Contact from "../models/contacts.js";
 import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find({}, "-createdAt -updatedAt");
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.getById(id);
+  const result = await Contact.findById(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -17,13 +17,13 @@ const getById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const removeContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.removeContact(id);
+  const result = await Contact.findByIdAndRemove(id);
   if (!result) {
     throw new HttpError(404, "Not found");
   }
@@ -32,7 +32,16 @@ const removeContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -45,4 +54,5 @@ export default {
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,17 +2,30 @@ import express from "express";
 import ctrl from "../../controllers/contacts.js";
 import validateBody from "../../decorators/validateBody.js";
 import shemas from "../../schemas/contacts.js";
+import isValidId from "../../middlewares/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", ctrl.listContacts);
 
-contactsRouter.get("/:id", ctrl.getById);
+contactsRouter.get("/:id", isValidId, ctrl.getById);
 
 contactsRouter.post("/", validateBody(shemas.addSchema), ctrl.addContact);
 
-contactsRouter.delete("/:id", ctrl.removeContact);
+contactsRouter.delete("/:id", isValidId, ctrl.removeContact);
 
-contactsRouter.put("/:id", validateBody(shemas.addSchema), ctrl.updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(shemas.addSchema),
+  ctrl.updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(shemas.updateStatusContactSchema),
+  ctrl.updateStatusContact
+);
 
 export default contactsRouter;
